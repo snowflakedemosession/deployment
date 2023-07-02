916bd4cf1c68ef234de7ee8cbc109f6909dc385c
python class <testpython@gmail.com>
2023-07-02 18:13:39 +0530

    project created

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(r):
-    return HttpResponse("<h1> Welocome to Scodeen </h1>")
+    return HttpResponse("<h1> Welcome to Scodeen </h1>")
